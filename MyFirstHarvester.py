0e37de79a9e38ca1c5f84c491b6ef39dbef94410
Tomas Dolezal <tomas@dolezal.it>
2021-03-18 12:07:30 +0100

    repaired mistakes, working version

--- MyFirstHarvester.py
+++ MyFirstHarvester.py
@@ -1,21 +1,58 @@
 #!/usr/bin/env python3
 
-# import datetime
+import datetime
 import numpy as np
 import bspump
 import bspump.common
 import bspump.file
 import bspump.trigger
+import pandas as pd
 
 
 class Harvester(bspump.Pipeline):
+
     def __init__(self, app, id=None, config=None):
         super().__init__(app, id, config)
 
-        self.Dataset = np.zeros(2, 288)
+        self.Resolution = 60 * 60  # 5min interval# 5min interval
+        self.MaxSample = int((24 * 60 * 60) / self.Resolution)
+        self.Dataset = np.zeros((3, self.MaxSample))
+        self.StartTime = datetime.datetime(year=2020, month=6, day=8)
 
     def process(self, context, event):
-        value = float(event['Value'])
+        time = datetime.datetime.strptime(
+            event['Time'],
+            '%Y-%m-%d %H:%M:%S'
+        )
+
+        # Delta time from StartTime
+        dt = (time - self.StartTime).total_seconds() / self.Resolution
+
+        # An early event
+        if dt < 0:
+            return event
+
+        # A late event
+        if dt >= self.MaxSample:
+            return event
+
+        try:
+            value = float(event['Value'])
+        except ValueError:
+            return event
+
+        dt = int(dt)
+
+        self.Dataset[0, dt] = self.Dataset[0, dt] + value
+        self.Dataset[1, dt] = self.Dataset[1, dt] + 1
+        self.Dataset[2, dt] = self.Dataset[0, dt] / self.Dataset[1, dt]
+
+        return event
+
+    def dump(self):
+        # Returns average value of incoming events
+        df = pd.DataFrame(self.Dataset[2])
+        return df
 
 
 class MyFirstPipeline(bspump.Pipeline):
@@ -30,7 +67,7 @@ class MyFirstPipeline(bspump.Pipeline):
 
         # Definition of my processors
         self.Print = bspump.common.PPrintProcessor(app, self)
-        #        self.Harvester = Harvester(app, self)
+        self.Harvester = Harvester(app, self)
 
         # Definition of my sink
         self.Sink = bspump.common.NullSink(app, self)
@@ -38,7 +75,7 @@ class MyFirstPipeline(bspump.Pipeline):
         # Definition of my pipeline
         self.build(
             self.Source,
-            self.Print,
+            # self.Print,
             self.Harvester,
             self.Sink
         )
@@ -47,8 +84,8 @@ class MyFirstPipeline(bspump.Pipeline):
         self.PubSub.subscribe("bspump.pipeline.cycle_end!", self.on_cycle_end)
 
     def on_cycle_end(self, event_name, pipeline):
-#        self.Harvester.dump()
-        print("We are done!")
+        df = self.Harvester.dump()
+        print(df)
         self.App.stop()
 
 
